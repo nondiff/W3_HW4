99f8819616d657c812ec23830465ba6cbedd5393
caici <caici@ASUS>
2024-10-16 21:47:20 +0800

    加入專案檔案。

---
+++ s1114760HW4/Controllers/ClothesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using s1114760HW4.Models;
+
+namespace s1114760HW4.Controllers
+{
+    public class ClothesController : Controller
+    {
+        private ClothEntities db = new ClothEntities();
+
+        // GET: Clothes
+        public ActionResult Index()
+        {
+            return View(db.Clothes.ToList());
+        }
+
+        // GET: Clothes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // GET: Clothes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clothes/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,名稱,尺寸,顏色,現貨")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clothes.Add(cloth);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cloth);
+        }
+
+        // GET: Clothes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // POST: Clothes/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,名稱,尺寸,顏色,現貨")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cloth).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cloth);
+        }
+
+        // GET: Clothes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // POST: Clothes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cloth cloth = db.Clothes.Find(id);
+            db.Clothes.Remove(cloth);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ s1114760HW4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="s1114760HW4.MvcApplication" Language="C#" %>
